486c931fe47ad2bc8673cf2c73cf03cbe291c241
asungkim <asungkim007@naver.com>
2025-02-25 19:20:24 +0900

    [feat] 백엔드 환경설정 #1

---
+++ backend/src/main/java/com/personal/asung_coffee_store/AsungCoffeeStoreApplication.java
@@ -0,0 +1,13 @@
+package com.personal.asung_coffee_store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsungCoffeeStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsungCoffeeStoreApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/personal/asung_coffee_store/AsungCoffeeStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personal.asung_coffee_store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsungCoffeeStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
